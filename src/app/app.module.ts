53650fd86f900a5f513f642280e1f8f9bfc9d8de
xAlexei51 <alexeipalacios12@gmail.com>
2023-11-16 19:06:45 -0600

    Login page withut responive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
